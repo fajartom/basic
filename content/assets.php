5d036ab2d68ffa3aa2168a69c0f7cc8beec6bfca
fajar <fajarqibs@gmail.com>
2020-03-26 01:24:10 +0700

    changes target link blank in assets

--- content/assets.php
+++ content/assets.php
@@ -3,15 +3,15 @@
         <div class="col-md-12">
             <div class="jumbotron">
                 <h1 class="display-4">Slider</h1>
-                    <p class="lead"><a class="" href="https://owlcarousel2.github.io/OwlCarousel2/">OwlCarousel</a></p>
-                    <p class="lead"><a class="" href="https://getbootstrap.com/docs/4.4/components/carousel/">Carousel</a></p>
+                    <p class="lead"><a class="" href="https://owlcarousel2.github.io/OwlCarousel2/" target="_blank">OwlCarousel</a></p>
+                    <p class="lead"><a class="" href="https://getbootstrap.com/docs/4.4/components/carousel/" target="_blank">Carousel</a></p>
                     
             </div>
         </div>
         <div class="col-md-6">
             <div class="jumbotron">
                 <h1 class="display-4">Navbar</h1>
-                <p class="lead"><a class="" href="https://bootstrapious.com/p/bootstrap-mega-menu/">Mega Menu</a></p>
+                <p class="lead"><a class="" href="https://bootstrapious.com/p/bootstrap-mega-menu/" target="_blank">Mega Menu</a></p>
             </div>
         </div>
         <div class="col-md-4">
