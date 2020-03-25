00b9bc2576488815abd4abfc21fe04fd29d0e38e
fajar <fajarqibs@gmail.com>
2020-03-26 03:10:20 +0700

    php tricks progress

--- content/assets.php
+++ content/assets.php
@@ -2,28 +2,30 @@
     <div class="row justify-content-md-center">
         <div class="col-md-12">
             <div class="jumbotron">
+                <h1 class="display-4">Reference</h1>
+                    <p class="lead"><a class="" href="https://javascript.info/" target="_blank">Javascript</a></p>
+                    <p class="lead"><a class="" href="https://getbootstrap.com/" target="_blank">Bootstrap CSS</a></p>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="jumbotron">
                 <h1 class="display-4">Slider</h1>
                     <p class="lead"><a class="" href="https://owlcarousel2.github.io/OwlCarousel2/" target="_blank">OwlCarousel</a></p>
                     <p class="lead"><a class="" href="https://getbootstrap.com/docs/4.4/components/carousel/" target="_blank">Carousel</a></p>
-                    
             </div>
         </div>
-        <div class="col-md-6">
+        <div class="col-md-4">
             <div class="jumbotron">
-                <h1 class="display-4">Navbar</h1>
+                <h1 class="display-4">Snippets</h1>
                 <p class="lead"><a class="" href="https://bootstrapious.com/p/bootstrap-mega-menu/" target="_blank">Mega Menu</a></p>
+                <p class="lead"><a class="" href="https://bootstrapious.com/snippets" target="_blank">Snippets Bootstrap</a></p>
             </div>
         </div>
         <div class="col-md-4">
             <div class="jumbotron">
-                <h1 class="display-4">Assets</h1>
-                    <p class="lead">Assets information.</p>
-            </div>
-        </div>
-        <div class="col-md-4">
-            <div class="jumbotron">
-                <h1 class="display-4">Assets</h1>
-                    <p class="lead">Assets information.</p>
+                <h1 class="display-4">Animation</h1>
+                    <p class="lead"><a class="" href="https://michalsnik.github.io/aos/">Aos</a></p>
+                    <p class="lead"><a class="" href="https://ianlunn.github.io/Hover/">Hover</a></p>
             </div>
         </div>
     </div>
--- index.php
+++ index.php
@@ -3,13 +3,20 @@ class main{
   function __construct(){
     include('template/header.php');
     include('template/menu.php');
+    /*
+    construct terpanggil duluan sebelum yang lain dipanggil.
+    kaya init gitu.
+    */
   } 
 
   function __destruct(){
     include('template/footer.php');
+    /*
+    destruct terpanggil paling belakangan setelah proses semua dipanggil
+    */
   }
 
-  public function getPage($page)
+  public function getPage($page) #variabel page disiapkan disini, namanya bebas gk harus page
   {
     if($page=='main') {
       include('content/main.php');
@@ -21,11 +28,18 @@ class main{
       include('content/php_tricks.php');
     }else{
       include('content/main.php');
-    }   
+    }
+    
+    //Ini contoh fungsi menampilkan halaman
+    
   }
 }
+#page mengambil parameter pada header get request
 $page = isset($_GET['page']) ? $_GET['page'] : 'main';
+
+#memanggil class main
 $main = new Main();
+
+#menginput $page ke parameter
 $main->getPage($page);
-return $main;
 ?>
